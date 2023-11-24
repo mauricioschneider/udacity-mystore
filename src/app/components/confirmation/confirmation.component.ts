fc529ae843341b0f5a3a7a542c6f6f8ea3465ab1
Mauricio Schneider <mauricio@schneider.xyz>
2023-11-24 09:01:43 -0800

    Add base components and service

---
+++ src/app/components/confirmation/confirmation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-confirmation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './confirmation.component.html',
+  styleUrl: './confirmation.component.css'
+})
+export class ConfirmationComponent {
+
+}
---
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-item-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-item-detail.component.html',
+  styleUrl: './product-item-detail.component.css'
+})
+export class ProductItemDetailComponent {
+
+}
